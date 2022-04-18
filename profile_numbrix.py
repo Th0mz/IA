6ad245d8074eef1d20da682713b399e5e22e33e8
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2022-04-18 11:28:44 +0100

    ctrl c with profiler now gives us all the info

--- profile_numbrix.py
+++ profile_numbrix.py
@@ -1,14 +1,25 @@
 import os
+import signal
 
-from matplotlib import lines
 from numbrix import main
 import cProfile
 import pstats
 
+def handler(signum, frame):
+    print ("Aborting ...")
+    filter_output()
+    exit()
+
+def filter_output():
+    with open("time.txt", "w") as f:
+        p = pstats.Stats("output.dat", stream=f)
+        p.sort_stats("time").print_stats()
+
+    os.remove("output.dat")
+ 
+ 
+signal.signal(signal.SIGINT, handler)
+
+
 cProfile.run("main()", "output.dat")
-
-with open("time.txt", "w") as f:
-    p = pstats.Stats("output.dat", stream=f)
-    p.sort_stats("time").print_stats()
-
-os.remove("output.dat")
+filter_output()
